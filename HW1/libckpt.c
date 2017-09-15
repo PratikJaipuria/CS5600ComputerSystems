c5c00afca8a6990e58f635cadb42dd61d9bb9c56
PratikJaipuria <jaipuriapratik@gmail.com>
2017-09-15 01:52:27 -0400

    loading libckpt and fetching proc/self/map

--- HW1/libckpt.c
+++ HW1/libckpt.c
@@ -8,7 +8,25 @@ void sighandler();
 __attribute__ ((constructor))
 void myconstructor()
 {
+	FILE *fptr;
+	char ch;
 	printf("DMTCP loaded...\n");
+	fptr = fopen("/proc/self/maps", "r");
+	
+    
+    if (fptr == NULL)
+    {
+        printf("Cannot open file \n");
+        exit(0);
+    }
+    ch = fgetc(fptr);
+    while (ch != EOF)
+    {
+        printf ("%c", ch);
+        ch = fgetc(fptr);
+    }
+    fclose(fptr);
+	
  	signal(SIGUSR2, sighandler);
 }
 
